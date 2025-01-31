7927bb2c14ee14dac666c4d2d6c3d76b3b33f670
Louis <70532216+l0uisgrange@users.noreply.github.com>
2025-01-31 15:47:43 +0100

    Start

---
+++ README.md
@@ -0,0 +1,5 @@
+## Commence par ici
+
+1. D'une fois que tu es prêt, accède à `src/routes/+page.svelte`
+
+Pour lancer le serveur, entre la commande `npm run dev` dans le terminal
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<!-- ICI TU PEUX METTRE TOUT LE CODE DONT TU AS BESOIN, EN UTILISANT DIRECTEMENT LES CLASSES TAILWIND -->
+
+<div class="bg-red-600"></div>
+
+<!-- POUR LANCER LE SERVEUR, ENTRE LA COMMANDE "npm run dev" -->
