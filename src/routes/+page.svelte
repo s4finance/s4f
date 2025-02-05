86f8dad88b95882c9dde30d1d9f43b3682725497
AKR-115 <126262976+AKR-115@users.noreply.github.com>
2025-02-06 00:01:27 +0100

    Test 1 on a template found on the internet.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<!--<h1>Welcome to SvelteKit</h1>-->
+<!--<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>-->
 
 <!-- ICI TU PEUX METTRE TOUT LE CODE DONT TU AS BESOIN, EN UTILISANT DIRECTEMENT LES CLASSES TAILWIND -->
 
 <div class="bg-red-600"></div>
 
 <!-- POUR LANCER LE SERVEUR, ENTRE LA COMMANDE "npm run dev" -->
+
+<!-- Test avec un truc trouvé sur internet-->
+
+<script>
+	import Nav from "./components/Nav.svelte";
+	import Hero from "./components/Hero.svelte";
+</script>
+
+<Nav />
+<Hero />
+
+<style>
+    body {
+        background: black;
+        color: white;
+        font-family: sans-serif;
+    }
+</style>
