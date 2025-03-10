c50fa25c4bb67e9861a60f9a326e63aa5153d327
AKR-115 <126262976+AKR-115@users.noreply.github.com>
2025-03-10 14:16:53 +0100

    Finished V1 of hero 5

--- src/routes/Hero5/+page.svelte
+++ src/routes/Hero5/+page.svelte
@@ -28,33 +28,42 @@
 			<Button flavor="light2" class="border-none">Enquire</Button>
 		</nav>
 		<div class="h-dvh grid grid-cols-10 grid-rows-5 gap-1 *:bg-black ">
-			<div class="size-full row-start-1 col-start-2 rounded-4xl animate-pop"></div>
-			<div class="size-full row-start-1 col-start-7 rounded-4xl"></div>
-			<div class="size-full row-start-1 col-start-8 rounded-4xl"></div>
+			<div class="size-full row-start-1 col-start-2 rounded-4xl animate-pop a-d-5"></div>
+			<div class="size-full row-start-1 col-start-7 rounded-4xl "></div>
+			<div class="size-full row-start-1 col-start-8 rounded-4xl animate-pop a-d-10"></div>
 			<div class="size-full row-start-1 col-start-9 rounded-4xl"></div>
-			<div class="size-full row-start-1 col-start-10 rounded-4xl"></div>
+			<div class="size-full row-start-1 col-start-10 rounded-4xl animate-pop a-d-8"></div>
 
 			<div class="size-full row-start-2 col-start-1 rounded-4xl"></div>
 			<div class="size-full row-start-2 col-start-3 rounded-4xl"></div>
-			<div class="size-full row-start-2 col-start-8 rounded-4xl"></div>
-			<div class="size-full row-start-2 col-start-9 rounded-4xl"></div>
+			<div class="size-full row-start-2 col-start-8 rounded-4xl animate-pop a-d-15"></div>
+			<div class="size-full row-start-2 col-start-9 rounded-4xl animate-pop a-d-7"></div>
 			<div class="size-full row-start-2 col-start-10 rounded-4xl"></div>
 
-			<div class="size-full row-start-3 col-start-4 rounded-4xl"></div>
-			<div class="size-full row-start-3 col-start-7 rounded-4xl"></div>
-			<div class="size-full row-start-3 col-start-10 rounded-4xl"></div>
+			<div class="size-full row-start-3 rounded-4xl animate-pop a-d-8"></div>
+			<div class="size-full row-start-3 col-start-4 rounded-4xl animate-pop a-d-15"></div>
+			<div class="size-full row-start-3 col-start-7 rounded-4xl animate-pop a-d-7"></div>
+			<div class="size-full row-start-3 col-start-10 rounded-4xl animate-pop a-d-3"></div>
 
 
-			<div class="size-full row-start-4 col-start-5 rounded-4xl"></div>
+			<div class="size-full row-start-4 rounded-4xl"></div>
+			<div class="size-full row-start-4 rounded-4xl animate-pop a-d-10"></div>
+			<div class="size-full row-start-4 col-start-5 rounded-4xl animate-pop a-d-8"></div>
 			<div class="size-full row-start-4 col-start-6 rounded-4xl"></div>
 			<div class="size-full row-start-4 col-start-10 rounded-4xl"></div>
 
-			<div class="size-full row-start-3 rounded-4xl"></div>
-			<div class="size-full row-start-4 rounded-4xl"></div>
-			<div class="size-full row-start-4 rounded-4xl"></div>
 			<div class="size-full row-start-5 rounded-4xl"></div>
 			<div class="size-full row-start-5 rounded-4xl"></div>
-			<div class="size-full row-start-5 rounded-4xl"></div>
+			<div class="size-full row-start-5 rounded-4xl animate-pop a-d-5"></div>
+		</div>
+		<div class="absolute top-0 left-0 px-10 grid grid-cols-5 grid-rows-5 h-dvh w-full">
+			<div class="m-5 p-4 text-center col-start-2 col-span-3 row-start-2 row-span-3 backdrop-blur-2xl bg-white/30 rounded-4xl">
+				<h1 class="text-8xl font-semibold">Automate Corporate Finance</h1>
+				<p class="mt-10 text-2xl">Precision-engineered solutions for those who <br> expect nothing but the best.</p>
+			</div>
+			<div class="row-start-5 col-start-3 flex justify-center items-center">
+				<Button flavor="light3">Let's meet</Button>
+			</div>
 		</div>
 	</section>
 
